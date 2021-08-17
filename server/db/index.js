0c630971c0bfb007ec830ca9eb7da673772e6088
Jian Qin <q46823155@gmail.com>
2021-08-17 22:45:13 +0800

    update

--- server/db/index.js
+++ server/db/index.js
@@ -14,7 +14,7 @@ const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD
 
 const proConfig = process.env.DATABASE_URL; //heroku addons
 
-const pool = new Pool({
+let pool = new Pool({
   connectionString:
     process.env.NODE_ENV === "production" ? proConfig : devConfig,
 });

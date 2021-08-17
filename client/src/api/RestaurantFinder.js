9fd95e2307b5c5d7d90a68949687b08142e05a22
Jian Qin <q46823155@gmail.com>
2021-08-17 23:42:22 +0800

    try

--- client/src/api/RestaurantFinder.js
+++ client/src/api/RestaurantFinder.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
-const devUrl = {
-  baseURL: "http://localhost:3005",
-};
+const baseURL =
+  process.env.NODE_ENV === "production" ? "" : "http://localhost:3005";
 
-const prodUrl = { baseURL: "https://rest-finder-xyz.herokuapp.com" };
-
-export default axios.create(
-  process.env.NODE_ENV === "production" ? prodUrl : devUrl
-);
+export default axios.create({
+  baseURL,
+});
